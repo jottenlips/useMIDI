2e301d2e15e99354ec66b63c22044ee92e7a8291
John Ottenlips <jottenlips@gmail.com>
2020-07-26 09:01:54 -0500

    types not working

--- packages/react-use-midi/src/useMIDI.ts
+++ packages/react-use-midi/src/useMIDI.ts
@@ -1,21 +1,26 @@
 import { useState } from "react";
-import WebMidi from "webmidi";
+import * as WebMidi from "webmidi";
 
 export const useMIDI = () => {
   const [inputs, setInputs] = useState();
   const [outputs, setOutputs] = useState();
   const [accessError, setAccessError] = useState();
 
+  // @ts-ignore
   WebMidi.enable((err) => {
     if (err) {
       setAccessError(err);
     }
+    // @ts-ignore
     setInputs(WebMidi.inputs);
+    // @ts-ignore
     setOutputs(WebMidi.outputs);
   });
 
   return {
+    // @ts-ignore
     getOutputById: WebMidi.getOutputById,
+    // @ts-ignore
     getInputById: WebMidi.getInputById,
     inputs,
     outputs,
