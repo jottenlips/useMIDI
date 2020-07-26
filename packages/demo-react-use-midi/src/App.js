1ba178bc4f31bd7cfb5c9c145f92a61a5e826a8c
John Ottenlips <jottenlips@gmail.com>
2020-07-25 22:53:40 -0500

    hoist success latest version of react

--- packages/demo-react-use-midi/src/App.js
+++ packages/demo-react-use-midi/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useMIDI } from "react-use-midi";
 function App() {
   return (
     <div className="App">
