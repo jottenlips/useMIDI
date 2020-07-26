ceafc823ef0eca9d7effd7dbab7e2a778901ab61
John Ottenlips <jottenlips@gmail.com>
2020-07-26 09:18:37 -0500

    Updates

---
+++ precache-manifest.5e903c716397b678b14902e9c78a8a23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae0f64f4c17de4dfb4ffe81201e092c",
+    "url": "/useMIDI/index.html"
+  },
+  {
+    "revision": "7be6956d91058cf90fa5",
+    "url": "/useMIDI/static/css/main.6dcb2595.chunk.css"
+  },
+  {
+    "revision": "7501a83d82fa616d3fe6",
+    "url": "/useMIDI/static/js/2.d4cab36e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/useMIDI/static/js/2.d4cab36e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be6956d91058cf90fa5",
+    "url": "/useMIDI/static/js/main.61c6a402.chunk.js"
+  },
+  {
+    "revision": "8cbc1f5e5faefc9ef34d",
+    "url": "/useMIDI/static/js/runtime-main.0da7132c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/useMIDI/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/usemidi/precache-manifest.de4a999de58ab31579e14f9237a6c521.js"
+  "/useMIDI/precache-manifest.5e903c716397b678b14902e9c78a8a23.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/usemidi/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/useMIDI/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
