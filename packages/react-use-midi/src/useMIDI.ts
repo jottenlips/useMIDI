d52eb73291dd90a4be8db50bd2c117d2da860a9f
John Ottenlips <jottenlips@gmail.com>
2020-07-26 00:28:29 -0500

    webmidi types throwing errors

--- packages/react-use-midi/src/useMIDI.ts
+++ packages/react-use-midi/src/useMIDI.ts
@@ -6,17 +6,21 @@ export const useMIDI = () => {
   const [outputs, setOutputs] = useState();
   const [accessError, setAccessError] = useState();
 
+  // @ts-ignore
   WebMidi.enable((err) => {
     if (err) {
       setAccessError(err);
     }
+    // @ts-ignore
     setInputs(WebMidi.inputs);
+    // @ts-ignore
     setOutputs(WebMidi.outputs);
   });
 
   return {
-    ...WebMidi,
+    // @ts-ignore
     getOutputById: WebMidi.getOutputById,
+    // @ts-ignore
     getInputById: WebMidi.getInputById,
     inputs,
     outputs,
