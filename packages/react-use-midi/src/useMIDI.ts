d98b303129b4c74128573222abe08744737b5572
John Ottenlips <jottenlips@gmail.com>
2020-07-26 08:59:52 -0500

    correct import

--- packages/react-use-midi/src/useMIDI.ts
+++ packages/react-use-midi/src/useMIDI.ts
@@ -1,26 +1,21 @@
 import { useState } from "react";
-import * as WebMidi from "webmidi";
+import WebMidi from "webmidi";
 
 export const useMIDI = () => {
   const [inputs, setInputs] = useState();
   const [outputs, setOutputs] = useState();
   const [accessError, setAccessError] = useState();
 
-  // @ts-ignore
   WebMidi.enable((err) => {
     if (err) {
       setAccessError(err);
     }
-    // @ts-ignore
     setInputs(WebMidi.inputs);
-    // @ts-ignore
     setOutputs(WebMidi.outputs);
   });
 
   return {
-    // @ts-ignore
     getOutputById: WebMidi.getOutputById,
-    // @ts-ignore
     getInputById: WebMidi.getInputById,
     inputs,
     outputs,
